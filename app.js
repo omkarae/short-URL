1c233e5792287e1e1e199cc6734226d45ab6821e
omkarae <omkaredgaonkar@gmail.com>
2022-11-26 17:51:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
+const app = express()
+
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+  
+  res.render('index')
+})
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  const shortUrl = await ShortUrl.findOne({ full: req.body.fullUrl });
+  res.render('short', { shortUrl: shortUrl });
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
+
+app.listen(process.env.PORT || 3000);
